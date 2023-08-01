a3efa134ca7c38c154c0fa5af67dfa58c4852ca4
dvnik147 <dvnik147@hotmail.com>
2023-08-01 14:17:01 +0800

    empty project add

---
+++ SwiftUI/learnTest/learnTest/learnTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnTestApp.swift
+//  learnTest
+//
+//  Created by 翊軒 on 2023/8/1.
+//
+
+import SwiftUI
+
+@main
+struct learnTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
